28fd0518cd52049bde204c1c3f2989ce35c6ea46
Mikecerc1 <Mikecerc1@outlook.com>
2022-03-20 19:23:30 -0400

    added making of config folders to bash script

--- install.bash
+++ install.bash
@@ -4,3 +4,13 @@ if [[ $USER != "root" ]]; then
     echo "Installer must be run as root!"
     exit 1
 fi
+
+if [[ ! -d "config/constants"]]; then
+    echo "constants folder doesn't exist, making one now"
+    mkdir -p "config/constants"
+fi 
+
+if [[ ! -d "config/auth"]]; then
+    echo "auth folder doesn't exist, making one now"
+    mkdir -p "config/auth"
+fi 
