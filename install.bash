506c76283ea518e910621a63660a16b7d74984bf
Mikecerc1 <Mikecerc1@outlook.com>
2022-03-20 21:02:15 -0400

    added creation of config files to install.bash

--- install.bash
+++ install.bash
@@ -14,3 +14,26 @@ if [[ ! -d "config/auth"]]; then
     echo "auth folder doesn't exist, making one now"
     mkdir -p "config/auth"
 fi 
+
+if [[ ! -f "config/constants/constants.json"]]; then   
+    echo "constants.json does not exist, creating it now"
+    echo "In minutes how often do you want to sync data with google sheets?"
+    echo "Please enter an integer"
+    read syncTime
+    touch "config/constants/constants.json"
+    echo "{ "SyncIntervalMinutes": "$syncTime" }" > "config/constants/constants.json"
+fi
+
+if [[ ! -f "config/constants/googleAPIConstants.json"]]; then   
+    echo "googleAPIConstants.json does not exist, creating it now"
+    echo "please enter the link to the google sheets you are logging your data in"
+    read sheetLink
+    touch "config/constants/googleAPIConstants.json"
+    echo "{ "scopes": ["https://www.googleapis.com/auth/spreadsheets"], "sheetURL": "$sheetLink" }" > "config/constants/googleAPIConstants.json"
+fi
+
+if [[ ! -f "config/constants/mongoDBConstants.json"]]; then 
+    echo "mongoDBConstants.json does not exist, creating it now"
+    touch "config/constants/mongoDBConstants.json"
+    echo { "mongodb://localhost:27017/test" } > "config/constants/mongoDBConstants.json"
+fi
