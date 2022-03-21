ce751725379630ed6f945b18c7a2c0fc82a0e308
Fritz Geib <fritz@standland.org>
2022-03-21 13:00:52 -0400

    fix root user check and remove some useless if statements

--- install.sh
+++ install.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-if [[ $USER != "root" ]]; then
+if [ "$EUID" -eq 0 ] ; then
     echo "Installer must be run as root!"
     exit 1
 fi
 
-if [[ ! -d "config/constants" ]]; then
-    echo "constants folder doesn't exist, making one now"
-    mkdir -p "config/constants"
-fi 
-
-if [[ ! -d "config/auth" ]]; then
-    echo "auth folder doesn't exist, making one now"
-    mkdir -p "config/auth"
-fi 
+#ensure some important directories exist
+mkdir -p "config/constants"
+mkdir -p "config/auth"
 
 if [[ ! -f "config/constants/constants.json" ]]; then   
     echo "constants.json does not exist, creating it now"
