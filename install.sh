07c6ca12c65b1c7baf38c024029585c0b6709d30
Fritz Geib <fritz@standland.org>
2022-03-21 12:41:15 -0400

    finish shellchecking should work now

--- install.sh
+++ install.sh
@@ -19,23 +19,23 @@ if [[ ! -f "config/constants/constants.json" ]]; then
     echo "constants.json does not exist, creating it now"
     echo "In minutes how often do you want to sync data with google sheets?"
     echo "Please enter an integer"
-    read syncTime
+    read -r syncTime
     touch "config/constants/constants.json"
-    echo "{ "SyncIntervalMinutes": "$syncTime" }" > "config/constants/constants.json"
+    echo "{ \"SyncIntervalMinutes\": $syncTime }" > "config/constants/constants.json"
 fi
 
 if [[ ! -f "config/constants/googleAPIConstants.json" ]]; then   
     echo "googleAPIConstants.json does not exist, creating it now"
     echo "please enter the link to the google sheets you are logging your data in"
-    read sheetLink
+    read -r sheetLink
     touch "config/constants/googleAPIConstants.json"
-    echo "{ "scopes": ["https://www.googleapis.com/auth/spreadsheets"], "sheetURL": "$sheetLink" }" > "config/constants/googleAPIConstants.json"
+    echo "{ \"scopes\": [\"https://www.googleapis.com/auth/spreadsheets\"], \"sheetURL\": \"$sheetLink\" }" > "config/constants/googleAPIConstants.json"
 fi
 
 if [[ ! -f "config/constants/mongoDBConstants.json" ]]; then 
     echo "mongoDBConstants.json does not exist, creating it now"
     touch "config/constants/mongoDBConstants.json"
-    echo "{ "mongoDBAddress": "mongodb://localhost:27017/test" }" > "config/constants/mongoDBConstants.json"
+    echo "{ \"mongoDBAddress\": \"mongodb://localhost:27017/test\" }" > "config/constants/mongoDBConstants.json"
 fi
 
 nvmVersion=$(nvm --version)
