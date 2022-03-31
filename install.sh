86358befd971d3c3da5c7acc142d7f4a653cdc40
WindowsVistaisCool <kyle@quackings.com>
2022-03-31 11:31:16 -0400

    shell args

--- install.sh
+++ install.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 
+# Screen defaults
+ADAFRUIT_SCREEN_SIZE="2.8"
+ADAFRUIT_TOUCH_ENABLED=false
+ADAFRUIT_TOUCH_RESISTIVE=true
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -t|--touch)
+      SEARCHPATH="$2"
+      shift # past argument
+      ;;
+    --toggle-touch-type )
+      SEARCHPATH="$2"
+      shift # past arg
+      ;;
+    --screen-size)
+      EXTENSION="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
 #ensure some important directories exist
 mkdir -p "config/auth"
 
@@ -7,7 +40,6 @@ mkdir -p "config/auth"
 if [[ ! -f ".env" ]]; then   
     echo ".env does not exist, creating it now"
     echo "In minutes how often do you want to sync data with google sheets?"
-    echo "Please enter an integer"
     read -r syncTime
     echo "# the interval at which the program will attempt to sync any database changes to google sheets." > ".env"
     echo "syncInterval=$syncTime " > ".env"
@@ -23,10 +55,10 @@ if [[ ! -f ".env" ]]; then
 fi
 
 # checks if nvm is installed, then installs it if it is not. 
-export NVM_DIR=$HOME/.nvm;
-source $NVM_DIR/nvm.sh;
-nvmVersion=$(nvm --version)
-if [[ $nvmVersion == "0.39.1" ]]; then
+# requires source ~/.profile to be run to ensure nvm is available
+export NVM_DIR=$HOME/.nvm
+source $NVM_DIR/nvm.sh
+if [[ $(nvm --version) == "0.39.1" ]]; then
     echo "NVM is installed, continuing..."
 else 
     wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
@@ -34,8 +66,7 @@ else
 fi 
 
 # checks if nodejs is installed, then installs it if not (with nvm)
-nodeVersion=$(node --version)
-if [[ $nodeVersion == "v16.14.2" ]]; then
+if [[ $(node --version) == "v16.14.2" ]]; then
     echo "Node is installed, continuing..."
 else 
     nvm install 16.14.2
