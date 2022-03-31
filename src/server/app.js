9fb714e0d39d3fbe8e3e6bb7f1875e3fd014106d
David Reeves <djreeves2005@gmail.com>
2022-03-31 11:16:10 -0400

    implement db functions to allow for signin and signout

--- src/server/app.js
+++ src/server/app.js
@@ -1,17 +1,20 @@
 const http = require('http')
 const fs = require('fs')
+// get functions to communicate to server
 const isSignedOut = require("../database/functions/isSignedOut.js");
+const submitData = require("../database/functions/submitDataByBatteryNumber.js");
+// run function app when ready
 async function app() {
   const server = http.createServer((req, res) => {
 
-    console.log("AAAA " + req.method + " " + req.headers["lrAction"])
+    // check if request is a GET request to get data or HTML
     if(req.method == "GET"){
 
-      // GETTING PAGE OR GETTING DATA
-      console.log(req.url);
+      // If the request is not going to the API, return the HTML file
       if(!req.url.includes("/api")){
         // get page
         fs.readFile("michael.html", function(err, data) {
+          // must specify diff. content type
           res.setHeader("Content-Type", "text/html");
           res.writeHead(200);
           res.end(data);
@@ -19,23 +22,23 @@ async function app() {
         });
         
       }else{
-        // get from API
-        console.log("GETTING DATA FROM API")
+        // data needs to be gotten from api
 
-
+        // all API data is JSON
         res.writeHead(200, { 'content-type': 'application/json' });
 
+        // first condition checking if API is looking for battery being signed out
         if(req.url.includes("isBatteryOut")){
           
+          // get the battery raw # and then turn it into a proper
           var batRaw = req.url.split("/")[req.url.split("/").length - 1];
           if(batRaw.length != 4){
             res.end(JSON.stringify({valid : false}));
           }
           var batNum = batRaw.substring(0,2) + "." + batRaw.substring(2,4);
           batNum = parseFloat(batNum);
-
+          // run the implemented function and handle the result back to the page
           isSignedOut(batNum).then(function(data){
-            console.log(data);
             if(data){
               res.end(JSON.stringify({valid : true, signedOut : data}));
             }else{
@@ -43,14 +46,34 @@ async function app() {
             }
           });
         }
-        //res.end(JSON.stringify({message : "Not Implemented"}));
+
         
       }
       
     }else{
-      // POSTING DATA
+      // Otherwise, request must be a POST
+      // all post data is JSON
       res.writeHead(200, { 'content-type': 'application/json' });
-      res.end(JSON.stringify({message : "Not Implemented"}));
+      // checking if the request is to submit data
+      if(req.url.includes("/submitsign")){
+        // needs to get chunks of data before ending the request
+        const chunks = [];
+        req.on('data', chunk => chunks.push(chunk));
+        req.on('end', () => {
+          // on end, parse the data
+          const data = JSON.parse(Buffer.concat(chunks));
+          //console.log(data);
+          // use implemented function to send the data
+          submitData(data["bNum"], data["rintBefore"], data["socBefore"], data["rintAfter"], data["socAfter"], data["purpose"], data["group"]).then(() => {
+            // once async function done, return a success
+            res.end(JSON.stringify({success : true}));
+          });
+          
+        })
+      }else{
+        res.end(JSON.stringify({message : "Not Implemented"}));
+      }
+      
     }
     
 
@@ -58,6 +81,7 @@ async function app() {
   })
 
 
+  // available at localhost:3000
 
   server.listen(3000)
 }
