596dbcdb34bf1f8c99c1efc2c58bcf43f3ba581d
David Reeves <djreeves2005@gmail.com>
2022-03-30 21:02:52 -0400

    implemented to send the right types of data back

--- src/server/app.js
+++ src/server/app.js
@@ -1,11 +1,41 @@
 const http = require('http')
 const fs = require('fs')
 async function app() {
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('../../michael.html').pipe(res)
-})
+  const server = http.createServer((req, res) => {
 
-server.listen(3000)
+    console.log("AAAA " + req.method + " " + req.headers["lrAction"])
+    if(req.method == "GET"){
+
+      // GETTING PAGE OR GETTING DATA
+
+      if(req.headers["lrAction"] == undefined){
+        // get page
+        fs.readFile("michael.html", function(err, data) {
+          res.setHeader("Content-Type", "text/html");
+          res.writeHead(200);
+          res.end(data);
+
+        });
+        
+      }else{
+        // get from API
+        res.writeHead(200, { 'content-type': 'application/json' });
+        res.end({data : "In Progress"});
+        
+      }
+      
+    }else{
+      // POSTING DATA
+      res.writeHead(200, { 'content-type': 'application/json' });
+      res.end({message : "Not Implemented"});
+    }
+    
+
+    
+  })
+
+
+
+  server.listen(3000)
 }
 module.exports = app;
