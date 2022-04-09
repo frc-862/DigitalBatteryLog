4dce69339d868b2b0482567bb2e746fb4fd53ba8
Mikecerc1 <Mikecerc1@outlook.com>
2022-04-09 10:16:01 -0400

    get logs

---
+++ src/database/functions/getLogs.js
@@ -0,0 +1,18 @@
+// returns all batteries signed in within the last 2 hours 
+const batteryDataModel = require('../../schemas/batterySchema.js')
+async function getSignedInLogs() {
+
+const currentTime = Date.now()
+//gets all singed In documents
+const res = await batteryDataModel.find();
+let finalDocs = [];
+//for every battery signed in, add it to the final values list ONLY if its only been signed back in for under 2 hours
+for (let doc in res) {
+    if (currentTime - Date.parse(res[doc].timeIn) >= 18000000 ) {
+        finalDocs.push(res[doc])
+    }
+}
+//returns an array of all docs that meet this requirement. 
+return finalDocs; 
+}
+module.exports = getSignedInLogs;   
