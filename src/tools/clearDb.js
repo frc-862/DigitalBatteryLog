4f76e2909f3f082bf6f2e92ad6f7073e9cb127fe
Mikecerc1 <Mikecerc1@outlook.com>
2022-04-09 14:10:46 -0400

    added tool for clearing db

---
+++ src/tools/clearDb.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const readline = require('readline');
+const batteryDataModel = require("../schemas/batterySchema.js");
+require('dotenv').config()
+
+try {
+    mongoose.connect(process.env.databaseAddress).then(() => { console.log('connected'); createDocument(); });
+} catch (err) {
+    if (err) console.error(err);
+}
+
+async function clearDb() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    console.log('Are you sure you want to delete all database entries')
+    rl.question('type Y for yes, anything else for No', (res) => {
+        if (res != 'Y') return;
+        batteryDataModel.deleteMany({}, (err) => {
+            if (err) console.error(err);
+        })
+    })
+}
